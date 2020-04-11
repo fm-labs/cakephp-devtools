b1607889cf06b9f8e435bbc5be5f37d88ca464e3
fm-labs <flowmotionlabs@gmail.com>
2020-04-11 16:05:36 +0200

    updated php-install script

--- scripts/php-install.sh
+++ scripts/php-install.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-PHP_VERSION="7.2"
+PHP_VERSION=$1
 
-sudo apt install curl
-sudo apt install php-curl php-xdebug
-sudo apt install php${PHP_VERSION}-cli \
+if [[ -z ${PHP_VERSION} ]]; then
+   echo "No php version selected. Selecting/ PHP version 7.2 by default"
+   PHP_VERSION="7.2"
+fi
+
+# Basic
+sudo apt install -y php${PHP_VERSION}-cli \
                   php${PHP_VERSION}-fpm \
                   php${PHP_VERSION}-intl  \
-                  php${PHP_VERSION}-pdo \
-                  php${PHP_VERSION}-pdo-sqlite \
-                  php${PHP_VERSION}-mcrypt \
-                  php${PHP_VERSION}-mbstring \
-                  php${PHP_VERSION}-curl
+                  php${PHP_VERSION}-common \
+                  php${PHP_VERSION}-sqlite3 \
+                  php${PHP_VERSION}-mbstring
+
+# Curl
+sudo apt install -y curl php-curl php${PHP_VERSION}-curl
+
+# Xdebug
+sudo apt install -y php-xdebug
+
+exit 0
