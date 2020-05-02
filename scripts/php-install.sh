6e31c3cf35556ceb2f9e9b853db8944f0b9e48e5
fm-labs <flowmotionlabs@gmail.com>
2020-05-02 17:03:44 +0200

    added soap package

--- scripts/php-install.sh
+++ scripts/php-install.sh
@@ -12,12 +12,13 @@ sudo apt install -y php${PHP_VERSION}-cli \
                   php${PHP_VERSION}-common \
                   php${PHP_VERSION}-intl \
                   php${PHP_VERSION}-mysql \
-                  php${PHP_VERSION}-sqlite \
+                  php${PHP_VERSION}-sqlite3 \
                   php${PHP_VERSION}-xml \
                   php${PHP_VERSION}-simplexml \
                   php${PHP_VERSION}-dom \
                   php${PHP_VERSION}-mbstring \
-                  php${PHP_VERSION}-json
+                  php${PHP_VERSION}-json \
+                  php${PHP_VERSION}-soap
 
 # Image processing
 sudo apt install -y php${PHP_VERSION}-gd
